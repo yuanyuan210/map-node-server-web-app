7b888414d31b62a9b4cbd95c79fb4ef5f1437588
Jia Xu <iamjiax@gmail.com>
2022-11-30 00:36:57 -0800

    reverse user db changes

--- users/users-controller.js
+++ users/users-controller.js
@@ -1,40 +1,14 @@
-// import people from './users.js'
+import people from './users.js'
 
-// let users = people
+let users = people
 
-// let currentUser = null
+let currentUser = null
 
-// const templateUser = {
-//     "username": ""
-// }
-import * as dao from './users-dao.js';
+const templateUser = {
+    "username": ""
+}
 
 const UserController = (app) => {
-
-    const createUser = async (req, res) => {
-        const newUser = req.body;
-        const actualUser = await dao.createUser(newUser);
-        res.json(actualUser);
-    }
-
-    const findAllUsers = async (req, res) => {
-        const users = await dao.findAllUsers();
-        res.json(users);
-    }
-
-    const deleteUser = (req, res) => {
-        const userId = req.params['uid'];
-        const status = dao.deleteUser(userId);
-        res.json(status);
-    }
-
-    const updateUser = (req, res) => {
-        const userId = req.params['uid'];
-        const updates = req.body;
-        const status = dao.updateUser(userId, updates);
-        res.json(status);
-    }
-    //*************
     const findUsers = (req, res) => {
         const role = req.query.role
         if (role) {
@@ -48,15 +22,45 @@ const UserController = (app) => {
     const findUserById = (req, res) => {
         const userId = req.params.uid;
         const user = users
-        .find(u => u._id === userId);
+            .find(u => u._id === userId);
         res.json(user);
     }
 
     const findUserByEmail = (req, res) => {
         const email = req.params.email;
         const user = users
-        .find(u => u.email === email);
+            .find(u => u.email === email);
         res.json(user);
+    }
+
+    const createUser = (req, res) => {
+        const newUser = req.body;
+        newUser._id = (new Date()).getTime() + '';
+        users.push(newUser);
+        res.json(newUser);
+    }
+
+    const deleteUser = (req, res) => {
+        const userId = req.params['uid'];
+        users = users.filter(usr =>
+            usr._id !== userId);
+        res.sendStatus(200);
+    }
+
+    const updateUser = (req, res) => {
+        const userId = req.params['uid'];
+        console.log(userId);
+        const updates = req.body;
+        const usr = users.find((usr) => usr._id === userId);
+        console.log(usr)
+        const newUser = {...usr, ...updates};
+        users = users.map((usr) =>
+            usr._id === userId ?
+                {...usr, ...updates} :
+                usr
+        );
+        console.log(newUser)
+        res.json(newUser);
     }
 
     const register = (req, res) => {
@@ -80,28 +84,28 @@ const UserController = (app) => {
             return
         }
 
+        currentUser = existingUser
         res.json(existingUser)
     }
 
     const logout = (req, res) => {
+        currentUser = null
         res.sendStatus(200)
     }
 
-
+    app.get('/api/users', findUsers);
     app.get('/api/users/:email', findUserByEmail);
 
     app.get('/api/users/:uid', findUserById);
 
-
+    app.post('/api/register', register);
     app.post('/api/users', createUser);
-    app.get('/api/users', findAllUsers);
     app.delete('/api/users/:uid', deleteUser);
     app.put('/api/users/:uid', updateUser);
 
-    app.post('/api/register', register);
     app.post('/api/login', login)
     app.post('/api/logout', logout)
 
 }
 
-export default UserController;
+export default UserController
--- users/users-model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import usersSchema from "./users-schema.js";
-
-const usersModel = mongoose.model('UsersModel', usersSchema);
-
-export default usersModel;
