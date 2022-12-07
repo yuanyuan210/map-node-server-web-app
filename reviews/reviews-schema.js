78e6e1e2eebf630460776d0bacc4f0f9742799f2
Jia Xu <iamjiax@gmail.com>
2022-12-06 20:49:53 -0800

    add reply to reviews schema

--- reviews/reviews-schema.js
+++ reviews/reviews-schema.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const reviewsSchema = new mongoose.Schema({
   place: {xid: String, name: String},
   user: {_id: String, username: String},
-  content: String
+  content: String,
+  reply: {
+    manager: {_id: String, username: String},
+    content: String
+  }
 }, {collection: "reviews"});
 
 export default reviewsSchema;
